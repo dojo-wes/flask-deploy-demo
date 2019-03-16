15a77d56503e45db2f2f97e09675ac33d807742c
Weston Harper <wes@tao.team>
2019-03-16 15:37:03 -0600

    initial commit

---
+++ app.py
@@ -0,0 +1,39 @@
+from flask import Flask, render_template
+from sqlalchemy.sql import func
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+app = Flask(__name__)
+# configurations to tell our app about the database we'll be connecting to
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///tweets.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# an instance of the ORM
+db = SQLAlchemy(app)
+# a tool for allowing migrations/creation of tables
+migrate = Migrate(app, db)
+
+class User(db.Model):
+    # __tablename__ = "users"    # optional
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(45))
+    email = db.Column(db.String(45))
+    created_at = db.Column(db.DateTime, server_default=func.now())    # notice the extra import statement above
+    updated_at = db.Column(db.DateTime, server_default=func.now(), onupdate=func.now())
+
+    def __repr__(self):
+        return f"<User username: {self.username}>"
+
+class Tweet(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    stuff = db.Column(db.String(240))
+    author_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+    author = db.relationship('User', foreign_keys=[author_id], backref="tweets", cascade="all")
+
+@app.route('/')
+def index():
+    return render_template(
+        'index.html',
+        user_list=User.query.all()
+    )
+
+if __name__ == "__main__":
+    app.run(debug=True)
